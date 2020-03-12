f6cdc82938d5d56d6e2bfdd4bdc60ba75a45817f
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2020-03-12 09:26:32 -0300

    mess

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Empregado;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var empr1 = new Empregado("Mariana Soares", "F");
+        }
+    }
+}
---
+++ Empregado/Empregado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Empregado
+{
+    public class Empregado
+    {
+        private string _nome;
+
+        public string Nome
+        {
+            get
+            {
+                if (Sexo == "M")
+                    return $"Sr.{_nome}";
+                else
+                    return $"Srª.{_nome}";
+            }
+            set { _nome = value; }
+        }
+
+        public string Sexo { get; private set; }
+
+        public Empregado(string nome, string sexo)
+        {
+            nome = Nome;
+            sexo = Sexo;
+        }
+    }
+}
